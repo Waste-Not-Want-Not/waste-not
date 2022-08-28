e172ce70f153ae3f45c66090bc6dace048388e86
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-08-28 16:42:55 -0700

    add basic mock data

---
+++ src/data/mock-data.js
@@ -0,0 +1,65 @@
+
+const data = {
+  "items": [
+    {
+      "id": 1,
+      "name": "Milk",
+      "image": "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png",
+      "location": "Fridge",
+      "expiration_date": "01/01/23",
+      "created_at": "08/28/23",
+      "days_till_expired": 16,
+      "for_donation":false
+    },
+    {
+      "id": 5,
+      "name": "Milk",
+      "image": "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png",
+      "location": "Fridge",
+      "expiration_date": "01/01/23",
+      "created_at": "08/28/23",
+      "days_till_expired": 16,
+      "for_donation":false
+    },
+    {
+      "id": 5,
+      "name": "Milk",
+      "image": "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png",
+      "location": "Fridge",
+      "expiration_date": "01/01/23",
+      "created_at": "08/28/23",
+      "days_till_expired": 16,
+      "for_donation":false
+    },
+    {
+      "id": 5,
+      "name": "Milk",
+      "image": "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png",
+      "location": "Fridge",
+      "expiration_date": "01/01/23",
+      "created_at": "08/28/23",
+      "days_till_expired": 16,
+      "for_donation":false
+    },
+    {
+      "id": 5,
+      "name": "Milk",
+      "image": "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png",
+      "location": "Fridge",
+      "expiration_date": "01/01/23",
+      "created_at": "08/28/23",
+      "days_till_expired": 16,
+      "for_donation":false
+    },
+    {
+      "id": 5,
+      "name": "Milk",
+      "image": "https://www.meijer.com/content/dam/meijer/product/0004/12/5010/20/0004125010200_2_A1C1_1200.png",
+      "location": "Fridge",
+      "expiration_date": "01/01/23",
+      "created_at": "08/28/23",
+      "days_till_expired": 16,
+      "for_donation":false
+    }
+  ]
+}
