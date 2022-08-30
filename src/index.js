96ee3d716626a1b513846e47f6cf9079b63c65b6
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-30 10:47:21 -0600

    Established ApolloProvider and client

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://waste-not-be.herokuapp.com/graphql'
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
