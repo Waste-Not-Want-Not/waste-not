112e80db46eebe76679ac1be99268aebf7e3c7ac
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-30 11:35:46 -0600

    getKitchen() function sorting items into correct loctations

--- src/components/Kitchen/Kitchen.js
+++ src/components/Kitchen/Kitchen.js
@@ -13,29 +13,43 @@ const Kitchen = () => {
     variables: { id: 1 }
   })
 
-  // const 
+  const getKitchen = (location) => {
+    const filteredItems = data.getUserById.items.filter(item => item.location === location);
+    const previews = filteredItems.map((item) => {
+      return <Preview key={item.name} item={item} />
+    })
+    return previews
+  }
 
-  return (
-    <section className='kitchen-container'>
-      {console.log(data)}
-      {/* <img src={require (Pantry)} alt='pantry'/>
-      <img src={require (Fridge)} alt='fridge'/>
-      <img src={require (Freezer)} alt='freezer'/> */}
-      <button>ADD NEW FOOD</button>
-      <article className='kitchen'>
-        <div className='pantry'>
-          <h3>Pantry</h3>
-        </div>
-        <div className='fridge'>
-          <h3>Fridge</h3>
-        </div>
-        <div className='freezer'>
-          <h3>Freezer</h3>
-        </div>
-      </article>
-      <button>SHOW POSSIBLE DONATIONS</button>
-    </section>
-  ) 
+  if (loading) {
+    return <p></p>
+  }
+  if (data) {
+    return (
+      <section className='kitchen-container'>
+        {/* {console.log(data.getUserById.items)} */}
+        {/* <img src={require (Pantry)} alt='pantry'/>
+        <img src={require (Fridge)} alt='fridge'/>
+        <img src={require (Freezer)} alt='freezer'/> */}
+        <button>ADD NEW FOOD</button>
+        <article className='kitchen'>
+          <div className='pantry'>
+            <h3>Pantry</h3>
+            {getKitchen('Pantry')}
+          </div>
+          <div className='fridge'>
+            <h3>Fridge</h3>
+            {getKitchen('Fridge')}
+          </div>
+          <div className='freezer'>
+            <h3>Freezer</h3>
+            {getKitchen('Freezer')}
+          </div>
+        </article>
+        <button>SHOW POSSIBLE DONATIONS</button>
+      </section>
+    ) 
+  }
 }
 
 export default Kitchen;
