0cfe607ace4d4e3454b194a970fc99d0aff64a2a
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-08-31 15:19:58 -0700

    build basic item card layout

---
+++ src/components/ItemCard/ItemCard.js
@@ -0,0 +1,13 @@
+const ItemCard = () => {
+    return (
+        <article className="item-card-container">
+            <p className="expiration">expires</p>
+            <div className="item-card">
+                <img />
+                <p>FOOD NAME PLACEHOLDER</p>
+                <button className="ate-button"></button>
+                <button className="donate-button"></button>
+            </div>
+        </article>
+    )
+}
