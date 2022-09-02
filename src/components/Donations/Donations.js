e739aa2fd9f31d04659b6c49f2cdda9a789a8d62
Rachel Bock <rachel.online@gmail.com>
2022-09-02 17:02:11 -0600

    Add Donation component to Donations Page

--- src/components/Donations/Donations.js
+++ src/components/Donations/Donations.js
@@ -1,12 +1,33 @@
 import 'Donations.css';
+import { useQuery } from '@apollo/client';
+import { GET_DONATION_ITEMS_QUERY } from '../../graphql/queries';
+
 
 const Donations = () => {
 
+  const {loading, data, error} = useQuery( GET_DONATION_ITEMS_QUERY, {
+    variables: {id: 1}
+  })
 
+  const getDonations = () => {
+    const donations = data.getUserById.donationItems.map((item) => {
+      return <Donation key={item.name} item={item} />
+    })
+    return donations
+  }
 
-  return(
-    <p>Yoda!</p>
-  )
+  if (error) return <h1>Technical difficulties, please visit us later.</h1>
+
+  if (loading) return <h2>LOADING...</h2>
+
+  if (data) {
+    return ( 
+      <section>
+        <h3>Donations</h3>
+        {getDonations()}
+      </section>
+
+    )
 
 }
 
--- src/components/DonationsPage/DonationsPage.js
+++ src/components/DonationsPage/DonationsPage.js
@@ -1,23 +1,13 @@
 import 'DonationsPage.css';
-import { useQuery } from '@apollo/client';
-import { GET_DONATION_ITEMS_QUERY } from '../../graphql/queries';
 
 const DonationsPage = () => {
-
-  const {loading, data, error} = useQuery( GET_DONATION_ITEMS_QUERY, {
-    variables: {id: 1}
-  })
-
-
-
-
 
 
 
 
   return (
     <FoodBankForm />
-    // {list of donations}
+    <Donations />
   )
 
 }
