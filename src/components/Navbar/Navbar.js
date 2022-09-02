74128526fb3d4c752a6f20e8351792d951dc1491
Rachel Bock <rachel.online@gmail.com>
2022-09-02 17:09:05 -0600

    Add NavLink to Navbar for donations page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,9 @@ const Navbar = () => {
         <button>HOME</button>
       </NavLink>
       <h1>Waste Not, Want Not</h1>
-      <button>DONATION PAGE</button>
+      <NavLink to='/donations'>
+        <button>DONATION PAGE</button>
+      </NavLink>
     </header>
   )
 }
