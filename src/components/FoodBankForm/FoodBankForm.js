d7276a342be96c2a41a3348dd3017a36dd550c7b
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-09-02 17:55:41 -0600

    Add query to FoodBankForm

--- src/components/FoodBankForm/FoodBankForm.js
+++ src/components/FoodBankForm/FoodBankForm.js
@@ -7,19 +7,23 @@ const FoodBankForm = () => {
 
   const [inputLocation, setLocation] = useState('');
 
+  const [getLocation, {loading, data, error}] = useLazyQuery(GET_FOODBANKS_QUERY, {
+    variables: { location: inputLocation }
+  })
+
   return (
     <section>
-      {console.log(inputLocation)}
+      {console.log(data)}
       <h3>Food Banks</h3>
-      <form>
+      <div>
         <input 
           type='text'
           placeholder='City, State'
           value={inputLocation}
           onChange={event => setLocation(event.target.value)}
         />
-        <button>Submit</button>
-      </form>
+        <button onClick={() => getLocation()}>Submit</button>
+      </div>
     </section>
   )
 
