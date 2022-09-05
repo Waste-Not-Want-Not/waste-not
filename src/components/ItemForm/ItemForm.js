a701618e756e07ddd8fcfca3091abad58e31ebf9
Rachel Bock <rachel.online@gmail.com>
2022-09-05 13:34:25 -0600

    Add Loading and Error handling to ItemForm Component Co-authored-by: Masaki <masakikleinkopf@gmail.com>

--- src/components/ItemForm/ItemForm.js
+++ src/components/ItemForm/ItemForm.js
@@ -11,43 +11,50 @@ const ItemForm = () => {
 
   const [createItem, {data, loading, error, refetch}] = useMutation(CREATE_ITEM);
 
-  return (
-    <section>
-      <h2>Update/Add Food</h2>
-      <form>
-        <input 
-          type='text'
-          placeholder='Item Name'
-          value={name}
-          onChange={event => setName(event.target.value)}
-        />
-        <select value={location} onChange={event => setLocation(event.target.value)}>
-          <option value='kitchen'>Select a location</option>
-          <option value='pantry'>Pantry</option>
-          <option value='fridge'>Fridge</option>
-          <option value='freezer'>Freezer</option>
-        </select>
-        <input 
-          type='date'
-          placeholder='Expiration Date/Expected Expiration Date'
-          value={date}
-          onChange={event => setDate(event.target.value)}
-        />
-        <button onClick={()=> {
-          createItem({ 
-            variables: {
-              input: {
-                userId: 1,
-                name,
-                location, 
-                expirationDate: `${date}T00:00:00Z`
+  if (error) return <h1>Technical difficulties, please visit us later.</h1>
+  
+  if (loading) return <h2>LOADING...</h2>
+  
+  if (data) {
+
+    return (
+      <section>
+        <h2>Update/Add Food</h2>
+        <form>
+          <input 
+            type='text'
+            placeholder='Item Name'
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+          <select value={location} onChange={event => setLocation(event.target.value)}>
+            <option value='kitchen'>Select a location</option>
+            <option value='pantry'>Pantry</option>
+            <option value='fridge'>Fridge</option>
+            <option value='freezer'>Freezer</option>
+          </select>
+          <input 
+            type='date'
+            placeholder='Expiration Date/Expected Expiration Date'
+            value={date}
+            onChange={event => setDate(event.target.value)}
+          />
+          <button onClick={()=> {
+            createItem({ 
+              variables: {
+                input: {
+                  userId: 1,
+                  name,
+                  location, 
+                  expirationDate: `${date}T00:00:00Z`
+                }
               }
-            }
-          })
-        }}>SUBMIT</button>
-      </form>
-    </section>
-  )
+            })
+          }}>SUBMIT</button>
+        </form>
+      </section>
+    )
+  }
 }
 
 export default ItemForm;
