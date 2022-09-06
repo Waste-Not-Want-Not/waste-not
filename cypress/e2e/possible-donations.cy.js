569581a91bedb9e61228ed01e4f5239b73dff399
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-09-06 12:32:59 -0600

    First test for Possible Donation page

--- cypress/e2e/possible-donations.cy.js
+++ cypress/e2e/possible-donations.cy.js
@@ -40,11 +40,59 @@ describe('Possible Donations Page', () => {
 
   beforeEach(() => {
     Cypress.config("interceptions", {});
-    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", {fixture: 'possibleDonations'});
+    const items = {
+        "data": {
+          "getUserById": {
+            "name": "Edward Schaden",
+            "email": "joetta.adams@wolf-grimes.name",
+            "items": [
+              {
+                "expirationDate": "2022-05-11T00:00:00Z",
+                "location": "fridge",
+                "name": "Cauliflower",
+                "forDonation": false
+              },
+              {
+                "name": "Cheetos",
+                "expirationDate": "2022-08-03T00:00:00Z",
+                "location": "pantry",
+                "forDonation": false
+              },
+              {
+                "expirationDate": "2022-08-17T00:00:00Z",
+                "location": "freezer",
+                "name": "Ice Cream",
+                "forDonation": false
+              },
+              {
+                "name": "Chocolate Bunnies",
+                "expirationDate": "2022-08-30T00:00:00Z",
+                "location": "pantry",
+                "forDonation": false
+              },
+              {
+                "expirationDate": "2022-08-31T00:00:00Z",
+                "location": "fridge",
+                "name": "Haloumi",
+                "forDonation": false
+              },
+              {
+                "name": "Plums",
+                "expirationDate": "2022-09-09T00:00:00Z",
+                "location": "freezer",
+                "forDonation": false
+              }
+            ]
+          }
+        }
+      }
+    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", items)
     cy.visit('http://localhost:3000/expiring')
   });
-
-  it('passes', () => {
-    cy.get('button').first().click()
+  
+  it('should have a Navbar with a heading, and three different navigation buttons', () => {
+    cy.get('.title').contains('Waste Not, Want Not')
+    cy.get('.navbar').find('button').should('have.length', 3)
+    cy.get('[href="/mykitchen"] > .nav-button').contains('MY KITCHEN')
   })
 })
