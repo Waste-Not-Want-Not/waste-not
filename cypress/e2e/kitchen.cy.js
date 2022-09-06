193f5659d889e6d10a751361e66852ea741938cd
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-09-06 12:46:26 -0700

    add intercept for createItem

--- cypress/e2e/kitchen.cy.js
+++ cypress/e2e/kitchen.cy.js
@@ -48,9 +48,47 @@ describe('kitchen', () => {
     cy.visit('http://localhost:3000')
   });
 
-  it('passes', () => {
+  it('should have correct navbar', () => {
     const items = data;
     cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", items);
     cy.get('button').first().click()
+    cy.get('.title').contains('Waste Not, Want Not')
+    cy.get('button').first().contains('MY KITCHEN')
+    cy.get('button').eq(1).contains('Overview')
+    cy.get('button').eq(2).contains('DONATION PAGE')
+  })
+
+  it('should have and be able to submit form', () => {
+
+    const items = data;
+
+    const newItem = {
+        name:"potatoes",
+        location:"pantry",
+        expirationDate:"2022-05-17T00:00:00",
+        userId:1,
+        image:"www.example.com",
+        forDonation:false,
+        id:Date.now()
+    }
+
+    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "createItem", {data:{
+        createItem:{
+            item: newItem
+        }
+    }})
+
+    const newItems = {...data}
+    newItems.data.getUserById.items.push(newItem)
+
+    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", items);
+    cy.get('button').first().click()
+    cy.get('input[type="text"]').type('potatoes')
+    cy.get('select').select('pantry')
+    cy.get('input[type="date"]').type('2022-05-17')
+    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", newItems);
+    cy.get('button').eq(3).click()
+    cy.wait(1000)
+    cy.get('.pantry').first().contains('potatoes')
   })
 })
