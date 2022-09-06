4c0b5053c2da2a6af2e7a151d43036152d248952
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-09-06 10:27:54 -0700

    add fixture

--- cypress/e2e/overview.cy.js
+++ cypress/e2e/overview.cy.js
@@ -1,5 +1,52 @@
+import data from '../fixtures/kitchen.json'
+
 describe('overview',() => {
+
+    Cypress.Commands.add(
+        "interceptGQL",
+        (
+          url,
+          operation,
+          data,
+          alias
+        ) => {
+          // Retrieve any previously registered interceptions.
+          const previous = Cypress.config("interceptions");
+          const alreadyRegistered = url in previous;
+      
+          const next = {
+            ...(previous[url] || {}),
+            [operation]: { alias, data },
+          };
+      
+          // Merge in the new interception.
+          Cypress.config("interceptions", {
+            ...previous,
+            [url]: next,
+          });
+      
+          // No need to register handler more than once per URL. Operation data is
+          // dynamically chosen within the handler.
+          if (alreadyRegistered) {
+            return;
+          }
+      
+          cy.intercept("POST", url, (req) => {
+            const interceptions = Cypress.config("interceptions");
+            const match = interceptions[url]?.[req.body.operationName];
+      
+            if (match) {
+              req.alias = match.alias;
+              req.reply({ body: match.data });
+            }
+          });
+        }
+      );
+
     beforeEach(() => {
+        Cypress.config("interceptions", {});
+        // const items = data;
+        // cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", items);
         cy.visit('http://localhost:3000/')
     })
 
