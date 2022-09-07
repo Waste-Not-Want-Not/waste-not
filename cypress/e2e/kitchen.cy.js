056674d003a0f6ecb064b56f787445936fde2210
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-09-06 17:17:40 -0700

    add tests for navigation

--- cypress/e2e/kitchen.cy.js
+++ cypress/e2e/kitchen.cy.js
@@ -67,7 +67,7 @@ describe('kitchen', () => {
     cy.get('.freezer').contains('Incaberries')
   })
 
-  it('should have and be able to submit form', () => {
+  it('should  able to create pantry item', () => {
 
     const items = data;
 
@@ -100,8 +100,72 @@ describe('kitchen', () => {
     cy.get('.pantry').first().contains('potatoes')
   })
 
-  it('should be able to go to pantry, fridge and freezer pages', () => {
-    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", data); //not working with multiple fetch calls
+  it('should  able to create fridge item', () => {
+
+    const items = data;
+
+    const newItem = {
+        name:"cheese",
+        location:"fridge",
+        expirationDate:"2022-09-17T00:00:00",
+        userId:1,
+        image:"www.example.com",
+        forDonation:false,
+        id:Date.now()
+    }
+
+    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "createItem", {data:{
+        createItem:{
+            item: newItem
+        }
+    }})
+
+    const newItems = {...items}
+    newItems.data.getUserById.items.push(newItem)
+
+    cy.get('button').first().click()
+    cy.get('input[type="text"]').type('cheese')
+    cy.get('select').select('fridge')
+    cy.get('input[type="date"]').type('2022-09-17')
+    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", newItems);
+    cy.get('button').eq(3).click()
+    cy.get('.fridge').first().contains('cheese')
+  })
+
+  it('should  able to create freezer item', () => {
+
+    const items = data;
+
+    const newItem = {
+        name:"Popsicles",
+        location:"freezer",
+        expirationDate:"2022-09-18T00:00:00",
+        userId:1,
+        image:"www.example.com",
+        forDonation:false,
+        id:Date.now()
+    }
+
+    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "createItem", {data:{
+        createItem:{
+            item: newItem
+        }
+    }})
+
+    const newItems = {...items}
+    newItems.data.getUserById.items.push(newItem)
+
+    cy.get('button').first().click()
+    cy.get('input[type="text"]').type('Popsicles')
+    cy.get('select').select('fridge')
+    cy.get('input[type="date"]').type('2022-09-18')
+    cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", newItems);
+    cy.get('button').eq(3).click()
+    cy.get('.freezer').first().contains('Popsicles')
+  })
+
+  it('should be able to go to pantry, fridge and freezer pages with correct urls', () => {
+    // cy.interceptGQL("https://waste-not-be.herokuapp.com/graphql", "getUserById", data); stubbin won't work here with multiple queries needed with interpolation
     cy.get('button').first().click()
     cy.get('.location-link').first().click()
     cy.url().should('eq', 'http://localhost:3000/pantry')
@@ -111,6 +175,10 @@ describe('kitchen', () => {
     cy.go('back')
     cy.get('.location-link').eq(2).click()
     cy.url().should('eq', 'http://localhost:3000/freezer')
+    cy.go('back')
+    cy.get('button').last().click()
+    cy.url().should('eq', 'http://localhost:3000/expiring')
+    
   })
 
   it('should show an error if error occurs during network request',() => {
