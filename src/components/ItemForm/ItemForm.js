34c973da4efeffdcc19836ac429fc424a12e2de4
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-09-07 11:25:58 -0600

    Add validation and styling to Item Form

--- src/components/ItemForm/ItemForm.js
+++ src/components/ItemForm/ItemForm.js
@@ -21,46 +21,54 @@ const ItemForm = ({refetch}) => {
     setDate('');
   }
 
-  const handleClick = () => {
-    createItem({ 
-      variables: {
-        input: {
-          userId: 1,
-          name,
-          location, 
-          expirationDate: `${date}T00:00:00Z`
-        }
-      },
-    });
-    refetch();
-    alert(`${name} was added to the ${location}!`)
-    clearInputs();
+  const handleClick = event => {
+    event.preventDefault();
+    if (name && location && date) {
+      createItem({ 
+        variables: {
+          input: {
+            userId: 1,
+            name,
+            location, 
+            expirationDate: `${date}T00:00:00Z`
+          }
+        },
+      });
+      refetch();
+      alert(`${name} was added to the ${location}!`)
+      clearInputs();
+    }
+    else {
+      alert('Please fill out all fields to add an item!')
+    }
   }
   
   return (
-    <section>
-      <h2>Update/Add Food</h2>
-      <div>
+    <section className='item-form-container'>
+      <h2>Add Item</h2>
+      <form onSubmit={(event) => handleClick(event)}>
         <input 
           type='text'
           placeholder='Item Name'
           value={name}
           onChange={event => setName(event.target.value)}
+          required
         />
         <select value={location} onChange={event => setLocation(event.target.value)}>
-          <option value='kitchen'>Select a location</option>
+          <option value=''>Select a location</option>
           <option value='pantry'>Pantry</option>
           <option value='fridge'>Fridge</option>
-          <option value='freezer'>Freezer</option>
+          <option value='freezer' >Freezer</option>
         </select>
         <input 
           type='date'
           placeholder='Expiration Date/Expected Expiration Date'
           value={date}
           onChange={event => setDate(event.target.value)}
+          required
         />
-        <button onClick={() => handleClick()}>SUBMIT</button>
-      </div>
+        <button>ADD FOOD</button>
+      </form>
     </section>
     )
   }
