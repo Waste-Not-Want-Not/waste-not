a1ca128ecfef68ff38358a653f975748658df76b
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-09-07 18:07:07 -0600

    Styling for Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import './Navbar.css';
+import Logo from '../../assets/app-logo.png';
+import Kitchen from '../../assets/kitchen-icon.png';
+import Overview from '../../assets/overview.png';
+import Donation from '../../assets/donation.png';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <header className='navbar'>
       <NavLink to='/mykitchen'>
-        <button className='nav-button'>MY KITCHEN</button>
+        <div className='nav-container'>
+          <img src={Kitchen} alt='kitchen logo' className='nav-button' />
+          <label>My Kitchen</label>
+        </div>
       </NavLink>
       <div className='nav-header'>
-        <h1 className='title'>Waste Not, Want Not</h1>
+        <img src={Logo} alt='app logo' className='app-logo' />
+        <h1 className='title'>WASTE NOT</h1>
+        <h1 className='title'>WANT NOT</h1>
         <NavLink to='/'>
-          <button className='overview-button'>Overview</button>
+          <div className='nav-container'>
+            <img src={Overview} alt='kitchen logo' className='nav-button' />
+            <label>Overview</label>
+          </div>
         </NavLink>
       </div>
       <NavLink to='/donations'>
-        <button className='nav-button'>DONATION PAGE</button>
+        <div className='nav-container'>
+          <img src={Donation} alt='kitchen logo' className='nav-button' />
+          <label>Donation Page</label>
+        </div>
       </NavLink>
     </header>
   )
